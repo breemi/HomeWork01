fc54a9fbcd7f1435ffa50b7fa5851f44a5a62424
Kirill Safronov <handofmaradona@MacBook-Air-Ruka.local>
2023-02-23 06:58:42 +0300

    Homework done

---
+++ Desktop/GeerBrains/языки програмирования/семинар/С# Project/HomeWork01/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает 
+// два числа и выдает, какое число большее, а какое меньшее.
+
+// System.Console.WriteLine("Введи первое число");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Введи второе число");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+// if (number1>number2){
+//     System.Console.WriteLine(number1);
+// }
+// else{
+//     System.Console.WriteLine(number2);
+// }
+
+// Задача 4: Напишите программу, которая принимает на вход 
+// три числа и выдает максимальное из этих чисел.
+
+// System.Console.WriteLine("Первое число");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Второе число");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Третье число");
+// int number3 = Convert.ToInt32(Console.ReadLine());
+// if (number1>number2 && number1>number3){
+//     System.Console.WriteLine(number1);
+// }
+// if (number2>number1 && number2>number3){
+//     System.Console.WriteLine(number2);
+// }
+// if (number3>number1 && number3>number2){
+//     System.Console.WriteLine(number3);
+// }
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдает, 
+// является ли число четным (делится ли оно на два без остатка).
+
+// System.Console.WriteLine("Введи число");
+// int number = Convert.ToInt32(Console.ReadLine());
+// if (number%2 == 0)
+// {
+//     System.Console.WriteLine("Чет");
+// }
+// else 
+// {
+//     System.Console.WriteLine("Нечет");
+// }
+
+// Задача 8: Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+
+// System.Console.WriteLine("Введи число");
+// int number = Convert.ToInt32(Console.ReadLine());
+// for (int i = 1; i <= number; i++)
+//     if (i%2 == 0){
+//         System.Console.Write(i+" ");
+//     }
+    
---
+++ Desktop/GeerBrains/языки програмирования/семинар/С# Project/Seminar 01/Task1/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая на вход принимает одно число (N), 
+// а на выходе показывает все целые числа в промежутке от -N до N.
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4" 
+// 2 -> " -2, -1, 0, 1, 2"
+
+// System.Console.WriteLine("Введите число");
+// int number = Convert.ToInt32(Console.ReadLine());
+// if (number<0){
+//     number = number * -1;
+// }
+// for(int i = -number; i <= number; i++)
+// {
+//     System.Console.Write(i+" ");
+// }
+
+//  Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает последнюю цифру этого числа.
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+
+// System.Console.WriteLine("Введите число  ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// if (number>99 && number<1000)
+// {
+//     int x = number%10;
+// Console.WriteLine(x);
+// }
+// else 
+// {
+//     System.Console.WriteLine("Введено невернео число");
+// }
+
+
+
